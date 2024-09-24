16e0ed449347c821b2339aa6f9b3ce91280a754d
0xf7ed0 <sausse.sylvain88@gmail.com>
2024-09-24 09:28:59 +0200

    optimised DGD

--- DGD_test.go
+++ DGD_test.go
@@ -1,19 +1,1 @@
 package sis
-
-import (
-	"fmt"
-	"testing"
-)
-
-func TestGaussianSumShouldBeOne(t *testing.T) {
-	d := NewDGD(100, 0.5)
-	fmt.Println(d)
-	sum := float64(0)
-	for _, item := range d {
-		sum += item
-	}
-	fmt.Println(sum)
-	if sum < 0.99 && sum > 1.01 {
-		t.Errorf("sum should be near 1 (<0.99) an is %v", sum)
-	}
-}
