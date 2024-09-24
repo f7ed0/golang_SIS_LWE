5c04bad7c884f94ab5ab736e6170f0dfbaf9dcb3
0xf7ed0 <sausse.sylvain88@gmail.com>
2024-09-24 09:28:41 +0200

    optimised DGD

--- DGD.go
+++ DGD.go
@@ -7,28 +7,24 @@ import (
 	"gonum.org/v1/gonum/stat/distuv"
 )
 
-type DiscreteGaussianDistribution []float64
+type DiscreteGaussianDistribution struct {
+	distuv.Normal
+}
 
 func NewDGD(q uint64, sigma float64) (d DiscreteGaussianDistribution) {
-	d = make(DiscreteGaussianDistribution, q)
-	s := rand.NewSource(uint64(time.Now().UnixMilli()))
-	normal := distuv.Normal{
+	d = DiscreteGaussianDistribution{distuv.Normal{
 		Mu:    float64(q / 2),
 		Sigma: sigma,
-		Src:   s,
-	}
-	for i := 0; i < int(q); i++ {
-		d[i] = normal.CDF(float64(i+1)) - normal.CDF(float64(i))
-	}
+		Src:   rand.NewSource(uint64(time.Now().UnixMilli())),
+	}}
 	return
 }
 
 func (d DiscreteGaussianDistribution) Rand() uint64 {
 	v := rand.Float64()
-	i := 0
-	for v > 0 {
-		v -= d[i]
+	i := 0.0
+	for v >= d.CDF(i) {
 		i++
 	}
-	return uint64(i)
+	return uint64(i - 1)
 }
